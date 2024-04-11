0c7ea67c91c42f6875f52bfd9be11ea51b4a95eb
Rajat Prajapati <rk22836@gmail.com>
2024-04-11 19:05:26 +0530

    Navigation bar and Bacground structuring and styling done

---
+++ script.js
@@ -0,0 +1,11 @@
+// const inputElement = document.getElementById('myInput');
+
+// inputElement.addEventListener('input', function (event) {
+//     // Call your function here
+//     myFunction(this.value);
+// });
+
+// function myFunction(value) {
+//     // Your function logic here
+//     console.log(value);
+// }
