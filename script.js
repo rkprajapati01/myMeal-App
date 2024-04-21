2c4f658fda5df2cd748074f32474c3adb0992396
Rajat Prajapati <rk22836@gmail.com>
2024-04-21 09:53:02 +0530

    favorite button functionality added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let favoriteList = [];
+
 const inputElement = document.getElementById('mealNameInput');
 
 inputElement.addEventListener('input', function (event) {
-    getMealData(this.value);
+    if (this.value !== "") {
+        getMealData(this.value);
+    }
 });
+
 
 
 function getMealData(refrenceText) {
@@ -26,20 +31,44 @@ function getMealData(refrenceText) {
 let mealCardContainerEl = document.getElementById('mealCardContainer');
 
 function createMealCard(mealData) {
+    console.log(mealData);
     let mealCardEl = document.createElement('div');
-    // mealCardEl.style.backgroundImage = `url(${mealData.strMealThumb})`;
     mealCardEl.classList.add('mealCard');
     let mealThumbEl = document.createElement('img');
     console.log(mealData.strMealThumb);
     mealThumbEl.setAttribute('src', `${mealData.strMealThumb}`);
     mealThumbEl.classList.add('mealThumb');
     let mealDescEl = document.createElement('div');
+    mealDescEl.classList.add('mealDesc');
 
     let mealNameEl = document.createElement('p');
-    mealNameEl.textContent = `${mealData.strMeal}`;
-    let favBtnEl = document.createElement('input')
-    favBtnEl.setAttribute('type', 'button');
-    mealDescEl.append(mealNameEl, favBtnEl);
+    mealNameEl.textContent = mealData.strMeal;
+    let favoriteButtonContainerEl = createFavButton();
+    mealDescEl.append(mealNameEl, favoriteButtonContainerEl);
+
     mealCardEl.append(mealThumbEl, mealDescEl);
     mealCardContainerEl.appendChild(mealCardEl);
-}   
+    console.log(mealCardEl);
+}
+
+function createFavButton() {
+    let favoriteButtonContainerEl = document.createElement('label');
+    favoriteButtonContainerEl.classList.add('favoriteButtonContainer');
+    let heartIconEl = document.createElement('i');
+    heartIconEl.classList.add('material-icons');
+    heartIconEl.textContent = 'favorite';
+    let favInputEl = document.createElement('input');
+    favInputEl.setAttribute('type', 'checkbox');
+    favoriteButtonContainerEl.append(favInputEl, heartIconEl);
+
+    favInputEl.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            heartIconEl.style.color = 'red';
+        }
+        event.target.disabled = true;
+    });
+    return favoriteButtonContainerEl;
+}
+
+
+
